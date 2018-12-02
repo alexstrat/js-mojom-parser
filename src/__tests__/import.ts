0999a707ab74b023b7510b49d95ba63fd5007f1c
Alexandre Lachèze <alexandre.lacheze@gmail.com>
2018-12-02 19:07:32 +0100

    Add some tests

---
+++ src/__tests__/import.ts
@@ -0,0 +1,42 @@
+import { getAST } from "../index";
+describe("Import statements", () => {
+  it("single statement", () => {
+    const mojo = `
+import "foo";
+    `;
+    expect(getAST(mojo)).toMatchInlineSnapshot(`
+Object {
+  "body": Array [
+    Object {
+      "filename": "foo",
+      "type": "ImportStatement",
+    },
+  ],
+  "type": "Mojom",
+}
+`);
+  });
+
+  it("double statement", () => {
+    const mojo = `
+import "foo";
+
+import "bar";
+    `;
+    expect(getAST(mojo)).toMatchInlineSnapshot(`
+Object {
+  "body": Array [
+    Object {
+      "filename": "foo",
+      "type": "ImportStatement",
+    },
+    Object {
+      "filename": "bar",
+      "type": "ImportStatement",
+    },
+  ],
+  "type": "Mojom",
+}
+`);
+  });
+});
